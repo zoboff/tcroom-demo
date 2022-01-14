e1aa8c00c8dc18c00a63f0e499539a899b6714ef
Andrey Zobov <zoboff@gmail.com>
2022-01-14 13:50:35 +0300

    refact simple_call.py

--- simple_call.py
+++ simple_call.py
@@ -3,6 +3,12 @@
 Created on 22.09.2020
 
 @author: zobov
+
+Command line:
+simple_call.py -room_ip <room IP> -trueconf_id <trueconf id>
+
+Example:
+python simple_call.py -room_ip "127.0.0.1" -trueconf_id "john@server.trueconf"
 '''
 
 from websocket import create_connection
@@ -14,12 +20,13 @@ request_unsecured = '''{
     "type" : "unsecured"
 }'''
 
+# calling a user (by trueconf_id) from TrueConf Room (by IP address)
 def simple_call(room_ip: str, trueconf_id: str):
     # create connection
-    ws = create_connection("ws://%s:8765" % room_ip)
+    ws = create_connection(f'ws://{room_ip}:8765')
     ws.send(request_unsecured)
     result =  ws.recv()
-    print("Received '%s'" % result)
+    print(f'Received: {result}')
     
     # make a command        
     command = {"method": "call", "peerId": trueconf_id}
@@ -27,7 +34,7 @@ def simple_call(room_ip: str, trueconf_id: str):
     ws.send(json.dumps(command))
     # result
     result =  ws.recv()
-    print("Result: '%s'" % result)
+    print(f'Result: {result}')
     
     # close connection
     ws.close()
